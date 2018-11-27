57a66ea7b94df1dfe21a6f1497bce30cb28e5d3e
Emika Ijuin <emikaijuin@gmail.com>
2018-11-27 17:44:42 +0800

    add column to store amenity category to differentiate between basic amenities, safety amenities, etc.

--- db/migrate/20181127092436_create_amenities.rb
+++ db/migrate/20181127092436_create_amenities.rb
@@ -2,6 +2,7 @@ class CreateAmenities < ActiveRecord::Migration[5.2]
   def change
     create_table :amenities do |t|
       t.string :description
+      t.integer :category
       t.timestamps
     end
   end
