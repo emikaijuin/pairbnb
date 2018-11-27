9aee8f0050c2ece7f9a7d111e1d77fb42bb19b14
Emika Ijuin <emikaijuin@gmail.com>
2018-11-27 17:24:27 +0800

    add property types and subtypes to migration

--- db/migrate/20181127075316_create_listings.rb
+++ db/migrate/20181127075316_create_listings.rb
@@ -1,15 +1,17 @@
 class CreateListings < ActiveRecord::Migration[5.2]
   def change
     create_table :listings do |t|
-      # Information
+      ## Information
       t.text :title
       t.text :description
-      # Capacity
+      t.text :property_type
+      t.text :property_subtype
+      ## Capacity
       t.integer :guests, index: true, order: {guests: :asc}
       t.integer :bedrooms, index: true, order: {bedrooms: :asc}
       t.integer :beds, index: true, order: {beds: :asc}
       t.float :bathrooms, index: true, order: {bathrooms: :asc}
-      # Location
+      ## Location
       t.text :address
       t.text :secondary_address
       t.text :city
@@ -18,8 +20,10 @@ class CreateListings < ActiveRecord::Migration[5.2]
       t.text :country
       # Owner
       t.belongs_to :user, index: true
+      ## Additional Details for Guests
+      t.text :details, array: true
 
-      t.timestamps, null: false
+      t.timestamps
     end
 
   end
