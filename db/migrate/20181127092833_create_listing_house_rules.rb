c81a9624e5c9659a535bc6fc6994b15514883a0e
Emika Ijuin <emikaijuin@gmail.com>
2018-11-27 17:33:06 +0800

    change migration to belong to listing and house rule

--- db/migrate/20181127092833_create_listing_house_rules.rb
+++ db/migrate/20181127092833_create_listing_house_rules.rb
@@ -1,7 +1,9 @@
 class CreateListingHouseRules < ActiveRecord::Migration[5.2]
   def change
     create_table :listing_house_rules do |t|
-      t.text :description
+      t.belongs_to :listing 
+      t.belongs_to :house_rule 
+      t.text :detail
       t.timestamps
     end
   end
