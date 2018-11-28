cf43519fb253b9d130d23e145b01acc5ed877268
Emika Ijuin <emikaijuin@gmail.com>
2018-11-28 17:04:18 +0800

    set placeholder method to return featured listings

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -6,4 +6,8 @@ class Listing < ApplicationRecord
 
   has_many :listing_house_rules
   has_many :house_rules, through: :listing_house_rules
+
+  def self.featured
+    Listing.all # this will change to trending listings based on reviews once review feature is implemented
+  end
 end
