1cdafd695289b1b3215cde634bc0b61553dbe28f
Emika Ijuin <emikaijuin@gmail.com>
2019-01-16 17:11:36 +0800

    root to listings index

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :users
+  
+  get "/listings/property_types" => "listings#property_types", as: "listing_property_types"
   resources :listings
-  root :to => "users#index"
+
+  root :to => "listings#index"
 end
