241fba4bb2d5bb3dad8088365fa361e6deb2dcf7
Emika Ijuin <emikaijuin@gmail.com>
2019-01-16 16:41:52 +0800

    add association to listing_images and add method to return available property types and subtypes

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -7,7 +7,35 @@ class Listing < ApplicationRecord
   has_many :listing_house_rules
   has_many :house_rules, through: :listing_house_rules
 
+  has_many :listing_images
+  has_many :images, through: :listing_images
+
   def self.featured
     Listing.all # this will change to trending listings based on reviews once review feature is implemented
+  end
+
+  def features
+    # Analyzes listing and returns things to be featured (e.g. superhost, only airbnb in area to have drying machine, etc.)
+    # Currently limiting to few potential features, future version may expand to include more
+    features = Hash.new
+    features[:superhost] = true if self.user.superhost?
+
+    # features[:high_rating] = true
+  end
+
+ 
+  def self.property_types
+    {
+      "Apartment" => ["Apartment", "Condo", "Casa Particular (Cuba)" ,"Loft", "Serviced Apartment"],
+      "Secondary Unit" => ["Guest House", "Guest Suite", "Homestay"],
+      "House" => ["House", "Bungalow", "Casa particular (Cuba)", "Chalet", "Cottage", "Cycladic House (Greece)", "Dammuso (Italy)", "Dome house", "Earth house", "Farm stay", "Houseboat", "Hut", "Lighthouse", "Pension (South Korea)", "Shepherd's hut (U.K., France)", "Tiny house", "Townhouse", "Trullo (Italy)", "Villa"],
+      "Unique Space" => ["Barn", "Boat", "Bus", "Camper/RV", "Campsite", "Castle", "Cave", "Dome house", "Earth house", "Farm stay", "Houseboat", "Hut", "Igloo", "Island", "Lighthouse", "Pension (South Korea)", "Plane", "Shepherd's hut (U.K., France)", "Tent", "Tiny house", "Windmill", "Yurt"],
+      "Bed and Breakfast" => ["Bed and Breakfast", "Casa particular (Cuba)", "Farm stay", "Minsu (Taiwan)", "Nature lodge", "Ryokan (Japan)"],
+      "Boutique Hotel" => ["Boutique Hotel", "Aparthotel", "Heritage hotel (India)", "Hostel", "Hotel", "Nature Lodge", "Resort", "Serviced apartment"]
+    }
+  end
+
+  def property_subtypes
+    {}
   end
 end
