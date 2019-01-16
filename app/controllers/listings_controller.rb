6feda0f172d6f1cc9229e7239b3effddb6ae9685
Emika Ijuin <emikaijuin@gmail.com>
2019-01-16 16:41:28 +0800

    create listings with strong params

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,18 +1,26 @@
 class ListingsController < ApplicationController
-  before_action :set_listing, except: [:index, :new, :create]
+  before_action :set_listing, except: [:index, :new, :create, :property_types]
 
   def index
     if params[:search]
       @listings = Listing.where(city: params[:city])
     else
-      @listings = Listing.featured
+      @listings = Listing.all
     end
   end
 
   def new
+    @listing = Listing.new
   end
 
   def create
+    @listing = current_user.listings.new(listing_params)
+    if @listing.save
+      Image.add_to_listing(@listing, params[:listing][:images]) # this shouldn't be in controller- how to refactor out of controller? Add to a service layer?
+      redirect_to listing_path(@listing)
+    else
+      render :new
+    end
   end
 
   def show
@@ -27,9 +35,39 @@ class ListingsController < ApplicationController
   def destroy
   end
 
+  # AJAX REQUESTS
+
+  def property_types
+		@property_subtypes = Listing.property_types[params[:property_type]]
+		respond_to do |format|
+			format.js {render :json => @property_subtypes }
+    end
+  end
+
   private
 
   def set_listing
     @listing = Listing.find(params[:id])
+  end
+
+  def listing_params
+    params.require(:listing).permit(
+     :images,
+     :title,
+     :description,
+     :property_type,
+     :property_subtype,
+     :guests,
+     :bedrooms,
+     :beds,
+     :bathrooms,
+     :address,
+     :secondary_address,
+     :city,
+     :state,
+     :postal_code,
+     :country,
+     :details
+     )
   end
 end
