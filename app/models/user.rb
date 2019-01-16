bbd844e5dd182f4300d3f167ee321349c0d7479a
Emika Ijuin <emikaijuin@gmail.com>
2019-01-16 17:10:22 +0800

    add basic validations for name and email and enum for host and status

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :listings
+
+  validates :first_name, presence: true 
+  validates :last_name, presence: true 
+  validates :email, presence: true
+
+  enum host: [:not_host, :host, :superhost]
+  enum status: [:customer, :admin, :superadmin]
 end
