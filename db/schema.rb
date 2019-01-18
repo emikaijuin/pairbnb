2b7d22f249a738d2d24e29a01f57795195cca167
Emika Ijuin <emikaijuin@gmail.com>
2019-01-18 10:52:54 +0800

    regenrate schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_07_101547) do
+ActiveRecord::Schema.define(version: 2019_01_17_095117) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2018_12_07_101547) do
     t.integer "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "icon"
   end
 
   create_table "house_rules", force: :cascade do |t|
